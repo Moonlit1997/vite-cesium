6a6e419caba4f12007b79c547dbe62549c34fe1b
Moonlit1997 <zhouxiyu1997@gmail.com>
2024-02-27 15:20:57 +0800

    init

---
+++ src/components/Left.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="left">left</div>
+</template>
+<style scoped>
+.left {
+  width: 200px;
+  height: 100%;
+  background-color: transparent;
+  position: absolute;
+  left: 0;
+  top: 0;
+  overflow: auto;
+  padding: 10px;
+  box-sizing: border-box;
+  border-right: 1px solid #e0e0e0;
+}
+</style>
---
+++ src/components/Right.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="right">right</div>
+</template>
+<style scoped>
+.right {
+  width: 200px;
+  height: 100%;
+  background-color: transparent;
+  position: absolute;
+  right: 0;
+  top: 0;
+  overflow: auto;
+  padding: 10px;
+  box-sizing: border-box;
+  border-left: 1px solid #e0e0e0;
+}
+</style>
